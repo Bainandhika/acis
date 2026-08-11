5941d2017ceb7c4b2b1d7d9e693a8e8da67fd073
M Bainandhika Baghaskara P <bainandhika.baghaskara@dansmultipro.com>
2026-08-12 01:07:30 +0700

    feat: implement auth domain HTTP handler

---
+++ apps/backend/internal/auth/handler.go
@@ -0,0 +1,49 @@
+package auth
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	svc Service
+}
+
+func NewHandler(svc Service) *Handler {
+	return &Handler{svc: svc}
+}
+
+func (h *Handler) RequestOTP(c *gin.Context) {
+	var req RequestOTPReq
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := h.svc.RequestOTP(c.Request.Context(), req); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "OTP code has been sent successfully"})
+}
+
+func (h *Handler) VerifyOTP(c *gin.Context) {
+	var req VerifyOTPReq
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	res, err := h.svc.VerifyOTP(c.Request.Context(), req)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "OTP verification successful",
+		"data":    res,
+	})
+}
