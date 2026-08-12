b4b868bae711bfd5ed6b0c39bd73a5945f9798da
M Bainandhika Baghaskara P <bainandhika.baghaskara@dansmultipro.com>
2026-08-12 08:31:15 +0700

    feat: add role field to UserResponse DTO

--- apps/backend/domain/authentication/dto.go
+++ apps/backend/domain/authentication/dto.go
@@ -13,6 +13,7 @@ type UserResponse struct {
 	ID        string  `json:"id"`
 	Email     string  `json:"email"`
 	Name      string  `json:"name"`
+	Role      string  `json:"role"`
 	AvatarURL *string `json:"avatar_url"`
 }
 
