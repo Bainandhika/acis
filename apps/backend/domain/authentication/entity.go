849024dbacf0ac8b35724aa5d0786a157123bb7b
M Bainandhika Baghaskara P <bainandhika.baghaskara@dansmultipro.com>
2026-08-16 21:57:41 +0700

    feat: update user entity struct to include username and phone number

--- apps/backend/domain/authentication/entity.go
+++ apps/backend/domain/authentication/entity.go
@@ -4,10 +4,9 @@ import "time"
 
 type User struct {
 	ID             string    `db:"id" json:"id"`
-	Email          string    `db:"email" json:"email"`
+	Username       string    `db:"username" json:"username"`
 	PhoneNumber    string    `db:"phone_number" json:"phone_number"`
 	Name           string    `db:"name" json:"name"`
-	GoogleID       *string   `db:"google_id" json:"google_id,omitempty"`
 	AvatarURL      *string   `db:"avatar_url" json:"avatar_url,omitempty"`
 	TelegramChatID *int64    `db:"telegram_chat_id" json:"telegram_chat_id,omitempty"`
 	CreatedAt      time.Time `db:"created_at" json:"created_at"`
